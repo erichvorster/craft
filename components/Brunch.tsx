3892ff816b6bd3d455101b15eedb965f00d5b874
erichvorster <erichvorster1@gmail.com>
2023-11-18 22:31:21 +0200

    updated itemCard

--- components/Brunch.tsx
+++ components/Brunch.tsx
@@ -3,7 +3,7 @@ import CategoryHeader from "./CategoryHeader";
 import { ItemDrawer } from "./ItemDrawer";
 import Image from "next/image";
 import ItemCard from "./ItemCard";
-import burger from "../public/burger-2.jpg";
+import burger from "../public/burger-2.jpeg";
 import MenuItems from "@/data/menu";
 
 const Brunch = () => {
@@ -15,7 +15,7 @@ const Brunch = () => {
         subText={"ONLY AVAILABLE BETWEEN 10:30 AND 13:00"}
       />
 
-      <div className="grid grid-cols-2 gap-4 mt-8">
+      <div className="grid grid-cols-2 gap-2 mt-8">
         {MenuItems.food.brunch.items.map((item, idx) => (
           <ItemDrawer food={item}>
             <ItemCard img={burger} food={item} />
