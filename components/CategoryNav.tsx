b04f9aabe2e725690707864277ca631609061c1b
erichvorster <erichvorster1@gmail.com>
2023-11-19 20:00:15 +0200

    added overlay effect

--- components/CategoryNav.tsx
+++ components/CategoryNav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import useScrollDirection from "./Hooks/useScrollDirection";
 
@@ -15,10 +15,71 @@ const CategoryNav = ({
   setActiveTab,
 }: CategoryHeaderProps) => {
   const scrollDirection = useScrollDirection();
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+  const [isScrolledToLeft, setIsScrolledToLeft] = useState(true);
+  const [isScrolledToRight, setIsScrolledToRight] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollContainerRef.current) {
+        const container = scrollContainerRef.current;
+        const isLeft = container.scrollLeft === 0;
+        const isRight =
+          container.scrollLeft + container.clientWidth ===
+          container.scrollWidth;
+
+        setIsScrolledToLeft(isLeft);
+        setIsScrolledToRight(isRight);
+      }
+    };
+
+    if (scrollContainerRef.current) {
+      const container = scrollContainerRef.current;
+      container.addEventListener("scroll", handleScroll);
+      return () => {
+        container.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
+
+  const handleScrollLeft = () => {
+    if (scrollContainerRef.current) {
+      const container = scrollContainerRef.current;
+      container.scrollTo({
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const handleScrollRight = () => {
+    if (scrollContainerRef.current) {
+      const container = scrollContainerRef.current;
+      container.scrollTo({
+        left: container.scrollWidth,
+        behavior: "smooth",
+      });
+    }
+  };
 
   return (
-    <div className="overflow-hidden w-full h-full mt-4 px-1">
-      <div className="overflow-x-scroll flex w-full h-full scrollbar-none">
+    <div className="overflow-hidden w-full h-full mt-4 px-1 relative">
+      <div
+        className="overflow-x-scroll flex w-full h-full scrollbar-none"
+        ref={scrollContainerRef}
+      >
+        <div
+          className={`w-12 bg-gradient-to-r from-transparent to-white absolute right-0 h-full z-50 ${
+            isScrolledToRight ? "hidden" : ""
+          }`}
+          onClick={handleScrollRight}
+        />
+        <div
+          className={`w-12 bg-gradient-to-r from-white to-transparent absolute left-0 h-full z-50 ${
+            isScrolledToLeft ? "hidden" : ""
+          }`}
+          onClick={handleScrollLeft}
+        />
         {tabs.map((tab, index) => (
           <motion.div
             key={index}
@@ -30,7 +91,7 @@ const CategoryNav = ({
             {activeTab === index + 1 && (
               <motion.span
                 layoutId="bubble"
-                className="absolute inset-0 z-0 bg-foreground mix-blend-difference rounded-lg p-2"
+                className="absolute inset-0 z-0 bg-foreground mix-blend-difference rounded p-2"
                 transition={{ type: "spring", bounce: 0.2, duration: 0.5 }}
               />
             )}
