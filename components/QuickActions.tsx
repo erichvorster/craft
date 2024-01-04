fc197f8eff67561b2ae29aff0c2a3cae1a563376
erichvorster <erichvorster1@gmail.com>
2024-01-04 21:05:27 +0200

    updated amplifty to gen2

--- components/QuickActions.tsx
+++ components/QuickActions.tsx
@@ -4,19 +4,19 @@ import { UtensilsCrossed, Beer, Gem, CalendarRange } from "lucide-react";
 const QuickActions = () => {
   return (
     <div className="grid grid-cols-2 gap-2 mb-24">
-      <div className="p-6 bg-card rounded-lg text-lg">
+      <div className="p-6 bg-popover-foreground rounded-lg text-lg text-popover">
         <UtensilsCrossed className="h-8 w-8 mb-4" />
         Food
       </div>
-      <div className="p-6 bg-card rounded-lg text-lg">
+      <div className="p-6 bg-popover-foreground rounded-lg text-lg text-popover">
         <Beer className="h-8 w-8 mb-4 " />
         Drinks
       </div>
-      <div className="p-6 bg-card rounded-lg text-lg">
+      <div className="p-6 bg-popover-foreground rounded-lg text-lg text-popover">
         <Gem className="h-8 w-8 mb-4" />
         Specials
       </div>
-      <div className="p-6 bg-card rounded-lg text-lg">
+      <div className="p-6 bg-popover-foreground rounded-lg text-lg text-popover">
         <CalendarRange className="h-8 w-8 mb-4 " />
         Events
       </div>
